4f13a4d4107d246a10d670074b8eddb51828025a
Aayush Prajapati <ashprajapati67@gmail.com>
2023-02-06 12:28:42 +0530

    Configuration added for invalid fields

---
+++ src/main/java/com/example/configuration/Serialization.java
@@ -0,0 +1,22 @@
+package com.example.configuration;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micronaut.context.event.ApplicationEventListener;
+import io.micronaut.runtime.server.event.ServerStartupEvent;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class Serialization implements ApplicationEventListener<ServerStartupEvent> {
+    private final ObjectMapper mapper;
+
+
+    public Serialization(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public void onApplicationEvent(ServerStartupEvent event) {
+        this.mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+    }
+}
