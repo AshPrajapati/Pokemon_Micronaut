ecaf0048a9c46a76021cbba8dbe051f56d2109bd
Paras Gediya <mrparas103@gmail.com>
2023-02-20 22:21:14 +0530

    refactor integration testing of power and pokemon api routes

--- src/test/java/com/example/pokemon/PokemonTest.java
+++ src/test/java/com/example/pokemon/PokemonTest.java
@@ -1,76 +1,103 @@
 package com.example.pokemon;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
 import com.example.power.Power;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.apache.http.HttpStatus;
+import org.assertj.core.api.AbstractStringAssert;
 import org.assertj.core.api.Assertions;
-import org.hamcrest.Matchers;
-import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 @MicronautTest
 public class PokemonTest {
-  Pokemon existingPokemon;
-  private ExtractableResponse<Response> res;
 
-  public PokemonTest() {
-    this.existingPokemon = new Pokemon(6, "Paras", new Power(3, "Bug"), "TestImageUrl");
+  private AbstractStringAssert<?> equalTo;
+
+  @BeforeEach
+  public void init() {
+    RestAssured.port = 5000;
   }
 
   @Test
-  public void addPokemon(RequestSpecification spec) {
-    spec.given()
-        .body("\"{\"name\":\"Balbasaur\",\"powerId\":3,\"imageUrl\":\"Pokemon Image\"}\"")
-        .header("Content-Type", "application/json")
-        .when()
-        .post("/pokemon")
-        .then()
-        .statusCode(HttpStatus.SC_CREATED)
-        .assertThat()
-        .body("name", Matchers.equalTo("Balbasaur"))
-        .body("power.id", Matchers.equalTo(3))
-        .body("image_url", Matchers.any(String.class));
+  public void addPokemon() {
+    PokemonForm newPokemonForm =
+        new PokemonForm("Balbasaur" + Math.random(), 1, "http://imageurl.com");
+    Pokemon createdPokemon =
+        given()
+            .contentType(ContentType.JSON)
+            .body(newPokemonForm)
+            .when()
+            .post("/pokemon")
+            .then()
+            .statusCode(HttpStatus.SC_CREATED)
+            .extract()
+            .as(new TypeRef<Pokemon>() {});
+    Assertions.assertThat(createdPokemon.getName()).isEqualTo(newPokemonForm.getName());
+    Assertions.assertThat(createdPokemon.getPower().getId()).isEqualTo(newPokemonForm.getPowerId());
+    Assertions.assertThat(createdPokemon.getImageUrl()).isEqualTo(newPokemonForm.getImageUrl());
+    Assertions.assertThat(createdPokemon.getId()).isNotNull();
   }
 
   @Test
-  public void getPokemon(RequestSpecification spec) {
+  public void getPokemon() {
+    String existingPokemonName = "Balbasaur";
     ExtractableResponse<Response> res =
-        spec.when().get("/pokemon").then().statusCode(HttpStatus.SC_OK).extract();
-    Assertions.assertThat(res.body().jsonPath().getList("name"))
-        .contains(existingPokemon.getName());
+        when().get("/pokemon").then().statusCode(HttpStatus.SC_OK).extract();
+    Assertions.assertThat(res.body().jsonPath().getList("name")).contains(existingPokemonName);
   }
 
   @Test
-  public void getPokemonById(RequestSpecification spec) {
-    spec.pathParam("id", existingPokemon.getId())
-        .when()
-        .get("/pokemon/{id}")
-        .then()
-        .statusCode(HttpStatus.SC_OK)
-        .assertThat()
-        .body("name", Matchers.equalTo(existingPokemon.getName()))
-        .body("power.id", Matchers.equalTo(existingPokemon.getPower().getId()));
+  public void getPokemonById() {
+    Pokemon existingPokemon =
+        new Pokemon(1, "Balbasaur", new Power(1, "Lightning"), "http://imageurl.com");
+    Pokemon foundPokemon =
+        given()
+            .pathParam("id", existingPokemon.getId())
+            .when()
+            .get("/pokemon/{id}")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .extract()
+            .as(new TypeRef<Pokemon>() {});
+    Assertions.assertThat(foundPokemon.getName()).isEqualTo(existingPokemon.getName());
+    Assertions.assertThat(foundPokemon.getId()).isEqualTo(existingPokemon.getId());
+    Assertions.assertThat(foundPokemon.getImageUrl()).isEqualTo(existingPokemon.getImageUrl());
   }
 
   @Test
-  public void updatePokemonById(@NotNull RequestSpecification spec) {
-    spec.pathParam("id", existingPokemon.getId())
-        .body("\"{\"name\":\"Balbasaur\"}\"")
-        .header("Content-Type", "application/json")
-        .when()
-        .put("/pokemon/{id}")
-        .then()
-        .statusCode(HttpStatus.SC_OK)
-        .assertThat()
-        .body("name", Matchers.equalTo("Balbasaur"));
+  public void updatePokemonById() {
+    int existingPokemonId = 2;
+    PokemonForm updatedPokemonForm = new PokemonForm("Pikachu", 1, "http://imageurl.com");
+    Pokemon updatedPokemon =
+        given()
+            .pathParam("id", existingPokemonId)
+            .body(updatedPokemonForm)
+            .contentType(ContentType.JSON)
+            .when()
+            .put("/pokemon/{id}")
+            .then()
+            .statusCode(HttpStatus.SC_CREATED)
+            .extract()
+            .as(new TypeRef<Pokemon>() {});
+    Assertions.assertThat(updatedPokemon.getName()).isEqualTo(updatedPokemon.getName());
+    Assertions.assertThat(updatedPokemon.getId()).isEqualTo(existingPokemonId);
+    Assertions.assertThat(updatedPokemon.getPower().getId())
+        .isEqualTo(updatedPokemonForm.getPowerId());
   }
 
   @Test
-  public void deletePokemonById(@NotNull RequestSpecification spec) {
-    spec.pathParam("id", existingPokemon.getId())
+  public void deletePokemonById() {
+    int existingPoekemonId = 5;
+    given()
+        .pathParam("id", existingPoekemonId)
         .when()
         .delete("/pokemon/{id}")
         .then()
--- src/test/java/com/example/power/PowerTest.java
+++ src/test/java/com/example/power/PowerTest.java
@@ -1,6 +1,12 @@
 package com.example.power;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -9,78 +15,80 @@ import org.apache.http.HttpStatus;
 import org.assertj.core.api.Assertions;
 import org.hamcrest.Matchers;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 @MicronautTest
 public class PowerTest {
 
-  private Power newPower;
-
-  public PowerTest() {
-    this.newPower = new Power(14, "Lightning");
+  @BeforeEach
+  public void init() {
+    RestAssured.port = 5000;
   }
 
   @Test
-  public void createNewPower(RequestSpecification spec) {
-    ExtractableResponse<Response> res =
-        spec.given()
-            .body("{\"name\":\"Lightning\"}")
-            .header("Content-Type", "application/json")
+  public void createNewPower() {
+    String newPowerName = "Lightning" + Math.random();
+    PowerForm powerForm = new PowerForm(newPowerName);
+    Power createdPower =
+        given()
+            .contentType(ContentType.JSON)
+            .body(powerForm)
             .when()
             .post("/powers")
             .then()
             .statusCode(HttpStatus.SC_CREATED)
-            .assertThat()
-            .body("name", Matchers.equalTo("Lightning"))
-            .body("id", Matchers.any(Integer.class))
-            .extract();
-    newPower =
-        new Power(res.body().jsonPath().getInt("id"), res.body().jsonPath().getString("name"));
+            .extract()
+            .as(new TypeRef<>() {});
+    Assertions.assertThat(createdPower.getName()).isEqualTo(newPowerName);
+    Assertions.assertThat(createdPower.getId()).isNotNull();
   }
 
   @Test
-  public void getAllPowers(RequestSpecification spec) {
-
+  public void getAllPowers() {
     ExtractableResponse<Response> res =
-        spec.when().get("/powers").then().statusCode(HttpStatus.SC_OK).extract();
+        when().get("/powers").then().statusCode(HttpStatus.SC_OK).extract();
     Assertions.assertThat(res.body().jsonPath().getList("name"))
-        .containsAnyElementsOf(List.of(newPower.getName()));
+        .containsAnyElementsOf(List.of("Lightning"));
   }
 
   @Test
-  public void getPowerById(RequestSpecification spec) {
-    spec.given()
-        .pathParam("id", newPower.getId())
+  public void getPowerById() {
+    Power existingPower = new Power(1, "Lightning");
+    given()
+        .pathParam("id", existingPower.getId())
         .when()
         .get("/powers/{id}")
         .then()
         .statusCode(HttpStatus.SC_OK)
         .assertThat()
-        .body("name", Matchers.equalTo(newPower.getName()))
-        .body("id", Matchers.equalTo(newPower.getId()));
+        .body("name", Matchers.equalTo(existingPower.getName()))
+        .body("id", Matchers.equalTo(existingPower.getId()));
   }
 
   @Test
-  public void updateById(RequestSpecification spec) {
+  public void updateById() {
+    PowerForm powerForm = new PowerForm("Ice");
+    int powerIdForUpdate = 9;
     ExtractableResponse<Response> res =
-        spec.given()
-            .pathParam("id", newPower.getId())
-            .body("{\"name\":\"UPDATEDNAME\"}")
-            .header("Content-Type", "application/json")
+        given()
+            .pathParam("id", powerIdForUpdate)
+            .contentType(ContentType.JSON)
+            .body(powerForm)
             .when()
             .put("/powers/{id}")
             .then()
             .statusCode(HttpStatus.SC_CREATED)
             .assertThat()
-            .body("name", Matchers.equalTo("UPDATEDNAME"))
+            .body("name", Matchers.equalTo("Ice"))
             .extract();
-    newPower.setName(res.body().jsonPath().getString("name"));
   }
 
   @Test
   public void deleteById(@NotNull RequestSpecification spec) {
+    int powerIdForDelete = 17;
     spec.given()
-        .pathParam("id", newPower.getId())
+        .pathParam("id", powerIdForDelete)
         .when()
         .delete("/powers/{id}")
         .then()
